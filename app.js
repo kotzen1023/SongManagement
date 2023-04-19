678b58e780e8329ad313eff86a37b58f43428041
Richie Shih <kotzen1023@gmail.com>
2023-04-19 01:02:30 +0000

    Richie commit

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var mainRouter = require('./routes/main');
+var uploadRouter = require('./routes/upload');
+var editsongRouter = require('./routes/editsong');
 
 var app = express();
 
@@ -34,6 +36,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/main', mainRouter);
+app.use('/upload', uploadRouter);
+app.use('/editsong', editsongRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
