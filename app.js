548c2c5b912ea4c49af8ff4b0960ee98bb2729c9
Richie Shih <kotzen1023@gmail.com>
2023-04-21 07:40:44 +0000

    Richie commit

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var loginRouter = require('./routes/login');
 var mainRouter = require('./routes/main');
 var uploadRouter = require('./routes/upload');
 var editsongRouter = require('./routes/editsong');
+var addsongRouter = require('./routes/addsong');
+var deletesongRouter = require('./routes/deletesong');
+var deleteAllRouter = require('./routes/deleteAll');
 
 var app = express();
 
@@ -22,11 +25,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({extended: false, limit: '50mb'}));
+//app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: false, parameterLimit: 50000 }))
 
 app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+//app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -38,6 +43,9 @@ app.use('/login', loginRouter);
 app.use('/main', mainRouter);
 app.use('/upload', uploadRouter);
 app.use('/editsong', editsongRouter);
+app.use('/addsong', addsongRouter);
+app.use('/deletesong', deletesongRouter);
+app.use('/deleteAll', deleteAllRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
